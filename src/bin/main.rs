f7fe4c360bc24b779e5c97e76bb7eb81d1fc3187
Joseph Catrambone <jo.jcat@gmail.com>
2021-08-21 12:11:23 -0700

    Set up iced and hammer out a prototype gui.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,125 +1,108 @@
-
-use macroquad as mq;
-use macroquad::prelude::*;
-use fooddb::FoodDB;
 use chrono::{NaiveDate, Datelike};
+use fooddb::FoodDB;
+use iced::*;
 
 const MAX_YEAR:i32 = 9999;
 
-struct AppState {
-	date_input: String,
+struct App {
 	active_year: i32,
-	active_month: u32,
-	active_day: u32,
+	active_month: i8,
+	active_day: i8,
 	food_db: FoodDB,
+
+	decrement_day: button::State,
+	increment_day: button::State,
 }
 
-#[macroquad::main("Calorie Counter")]
-async fn main() {
-	set_pc_assets_folder("assets");
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+	DecrementDay,
+	IncrementDay,
+	JumpToDate,
+	AddFood,
+}
 
-	//let mut food_db = FoodDB::from_string(mq::file::load_string(""));
-	/*
-	let mut food_db = FoodDB::default();
-	{
-		let mut fid = food_db.new_food();
-		fid.name = "TestFood".to_string();
-		fid.manufacturer = "TestManufacturer".to_string();
-		fid.volume_of_100g = 100.0;
-		fid.servings_in_100g = 1.0;
-		fid.nutrition.calories = 0;
-		fid.nutrition.carbohydrates = 0.0;
-		fid.nutrition.fats = 0.0;
-		fid.nutrition.proteins = 0.0;
+impl Application for App {
+	type Executor = executor::Default;
+	type Message = Message;
+	type Flags = ();
+
+	fn new(_flags: ()) -> (App, Command<Message>) {
+		let mut app = App {
+			active_year: 2000,
+			active_month: 0,
+			active_day: 0,
+			food_db: FoodDB::new(),
+			decrement_day: button::State::new(),
+			increment_day: button::State::new()
+		};
+		(app, Command::none())
 	}
-	*/
-	let mut food_db = FoodDB::from_string(include_str!("assets/default.fdb")).unwrap();
-	food_db.reindex();
-	food_db.save("food_db.fdb");
 
-	let mut app = AppState {
-		date_input: "".to_string(),
-		active_year: 1970,
-		active_month: 1,
-		active_day: 1,
-		food_db
-	};
+	fn title(&self) -> String {
+		String::from("Menu - Iced")
+	}
 
-	loop {
-		clear_background(WHITE);
+	fn view(&mut self) -> Element<Message> {
+		Container::new(
+			Column::new()
+				.push(
+					// Top-row date picker and settings wheel.
+					Row::new()
+						.push(
+							Button::new(&mut self.decrement_day, Text::new("<"))
+								.on_press(Message::DecrementDay)
+						)
+						.push(
+							Text::new(format!("{}/{}/{}", self.active_year, self.active_month, self.active_day)).size(30)
+						)
+						.push(
+							Button::new(&mut self.increment_day, Text::new(">"))
+								.on_press(Message::IncrementDay)
+						)
+				)
+				.push(
+					Text::new("Breakfast").size(50),
+				)
+				.push(
+					Text::new("Second Breakfast").size(50),
+				)
+				.push(
+					Text::new("Lunch").size(50),
+				)
+				.push(
+					Text::new("Snack").size(50),
+				)
+				.push(
+					Text::new("Dinner").size(50),
+				)
+		)
+			.width(Length::Fill)
+			.height(Length::Fill)
+			.center_x()
+			.center_y()
+			.into()
 
-		// Process keys, mouse etc.
+	}
 
-		// Configure and display UI.
-		egui_macroquad::ui(|egui_ctx| {
-			egui::CentralPanel::default()
-			//egui::Window::new("Calorie Counter")
-				.show(egui_ctx, |ui| {
-					//egui::menu::menu(ui, "Title!", |ui|{
+	fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
+		match message {
+			Message::DecrementDay => {
+				//self.value += 1;
+			},
+			Message::IncrementDay => {
+				//self.value -= 1;
+			},
+			Message::JumpToDate => {
 
-					//ui.heading("My egui Application");
-					ui.label(format!("Food Diary for {}/{}/{}", &app.active_year, &app.active_month, &app.active_day));
+			},
+			Message::AddFood => {}
+		}
 
-					/*
-					ui.horizontal(|ui| {
-						ui.label("Your name: ");
-						//ui.text_edit_singleline(&mut name);
-					});
-					*/
-
-					// Pick Date.
-					ui.horizontal(|ui|{
-						ui.text_edit_singleline(&mut app.date_input);
-						if(app.date_input)
-						/*
-						ui.add(egui::Slider::new(&mut app.active_year, 1970..=MAX_YEAR).text("Year"));
-						if ui.button("+").clicked() { app.active_year += 1; }
-						if ui.button("-").clicked() { app.active_year -= 1; } // Should address underflow.
-
-						ui.add(egui::Slider::new(&mut app.active_month, 1..=12).text("Month"));
-						if ui.button("+").clicked() { app.active_month += 1; }
-						if ui.button("-").clicked() { app.active_month -= 1; } // Should address underflow.
-
-						let days = days_in_month(app.active_year, app.active_month) as u32;
-						ui.add(egui::Slider::new(&mut app.active_day, 1..=days).text("Day"));
-						if ui.button("+").clicked() { app.active_day += 1; }
-						if ui.button("-").clicked() { app.active_day -= 1; } // Should address underflow.
-						*/
-					});
-
-					//ui.label(format!("Hello '{}', age {}", name, age));
-
-					//});
-				});
-		});
-
-		// Draw things before egui
-
-		// Draw the UI.
-		egui_macroquad::draw();
-
-		// Draw things after egui
-
-		// Advance.
-		next_frame().await;
+		Command::none()
 	}
 }
 
-fn date_picker(ui: &mut egui::Ui, app_state: &mut AppState) {
-
-}
-
-/// Given a year and a month (with Jan = 1), return the number of days in the given month.
-fn days_in_month(year:i32, month:u32) -> u8 {
-	let next_date_year = if month == 12 {
-		year+1
-	} else {
-		year
-	};
-	let next_date_month = if month == 12 {
-		1
-	} else {
-		month + 1
-	};
-	NaiveDate::from_ymd(next_date_year, next_date_month, 1).signed_duration_since(NaiveDate::from_ymd(year, month, 1)).num_days() as u8
+pub fn main() -> iced::Result {
+	App::run(Settings::default())
 }
